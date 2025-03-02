1df7bceb97e5852dc6e7688795c7b842131f4a9c
Mohd. Suleman <sulemnmohd345@gmail.com>
2025-03-03 02:25:45 +0530

    built the structure, using vite/react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BackgroundLines } from './components/ui/background-lines'
+// import { HeroParallaxDemo } from './components/ui/Hero-parallax-Demo'
+
+function App() {
+  return (
+    <>
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+    </BackgroundLines>
+   {/* <HeroParallaxDemo></HeroParallaxDemo> */}
+   
+    </> 
+  )
+}
+
+export default App
